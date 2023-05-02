5ad7fdb29e296d1fe178cd918ee16c88c5a07c88
Kandy/sh <kandarpsingh290@gmail.com>
2023-05-02 15:08:15 +0530

    Create Hackathon.cs

---
+++ Hackathon.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+
+namespace SampleConApp
+{
+    internal class StringEncoder
+    {
+        private const string key = "the quick and brown fox jumps over the lazy dog";
+        private static Dictionary<char, int> keyValuePairs = new Dictionary<char, int>();
+
+        private static void Main(string[] args)
+        {
+            string text = UIConsole.GetString("Enter your string you wish to Encode");
+
+            Console.WriteLine(Encoder(text.ToLower()));
+        }
+
+        private static string Encoder(string text)
+        {
+            DictionaryMaker();
+            if (text?.Length == 0)
+            {
+                throw new ArgumentException("Inavlid argument length");
+            }
+            if (text == null)
+            {
+                throw new NullReferenceException("text cannot be null");
+            }
+            string res = "";
+            int i = 0;
+            foreach (var item in text)
+            {
+                if (item == ' ')
+                {
+                    res += "-";
+                    continue;
+                }
+                if (keyValuePairs[item] < 10)
+                {
+                    res += $"0{keyValuePairs[item]}";
+                }
+                else
+                {
+                    res += $"{keyValuePairs[item]}";
+                }
+
+                if (i != text.Length - 1)
+                {
+                    var nextItem = text[i + 1];
+                    if (!nextItem.Equals(' '))
+                    {
+                        res += ",";
+                    }
+                }
+                i++;
+            }
+            return res.TrimEnd(',');
+        }
+
+        private static void DictionaryMaker()
+        {
+            int word = 0;
+            int place = 0;
+            foreach (var c in key)
+            {
+                if (c.Equals(' '))
+                {
+                    word++;
+                    place = 0;
+                    continue;
+                }
+                if (keyValuePairs.ContainsKey(c)) { place++; continue; }
+                keyValuePairs[c] = word * 10 + place;
+                place++;
+            }
+        }
+    }
+}
